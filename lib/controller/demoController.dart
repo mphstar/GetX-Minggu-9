d1436bfba905a1cc91c2458567f93ea26695a45c
mphstar <bintang.simdig20@gmail.com>
2022-11-11 07:09:06 +0700

    Upload

---
+++ lib/controller/demoController.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import '/model/product.dart';
+import 'package:get/get.dart';
+
+class DemoController extends GetxController{
+  var cartItems = <Product>[].obs;
+  int get cartCount => cartItems.length;
+  double get totalAmount => cartItems.fold(0.0, (sum, element) => sum + element.price);
+  addToCart(Product product){
+    cartItems.add(product);
+  }
+
+  resetCart() => cartItems.clear();
+
+
+  final storage = GetStorage();
+  bool get isDark => storage.read('darkmode') ?? false;
+  ThemeData get theme => isDark ? ThemeData.dark() : ThemeData.light();
+  void changeTheme(bool val) => storage.write('darkmode', val);
+}
